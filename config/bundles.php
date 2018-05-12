86fa62812a4bf9c59bddd8f410aa670ea8cc59b3
juan <mrpandat@hotmail.fr>
2018-05-12 13:21:39 +0200

    Add FOSUserBundle.

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];
